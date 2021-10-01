f94bb3f3da7d4682597de20cf4e0ee819f556387
boypt <1033514+boypt@users.noreply.github.com>
2021-10-01 22:16:10 +0800

    clean code

--- cmd/aria2addtask/main.go
+++ cmd/aria2addtask/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"net/url"
 	"os"
 	"time"
@@ -19,48 +18,41 @@ var (
 	dlname  string
 )
 
+func testAria2(c *aria2rpc.Aria2RPC) {
+	v, err := c.GetVersion()
+	fmt.Println(v, err)
+	s, err := c.GetGlobalStat()
+	fmt.Println(s, err)
+}
+
 func main() {
 	flag.StringVar(&rpc, "rpc", "http://localhost:6800", "aria2 rpc")
 	flag.StringVar(&token, "token", "", "aria2 token")
 	flag.StringVar(&uribase, "baseuri", "", "uri base")
 	flag.StringVar(&dlname, "dl", "", "dlname")
 	flag.BoolVar(&testrpc, "testrpc", false, "test rpc")
-
 	flag.Parse()
 
-	c := aria2rpc.NewAria2RPC(token, rpc)
-
+	aria2Client := aria2rpc.NewAria2RPC(token, rpc)
 	if testrpc {
-		ver, err := c.GetVersion()
-		if err != nil {
-			log.Panic(err)
-		}
-		println(ver)
-		s, err := c.GetGlobalStat()
-		if err != nil {
-			log.Panic(err)
-		}
-		fmt.Println(s)
+		testAria2(aria2Client)
 		os.Exit(0)
 	}
 
-	dlUrl := ""
 	if c, ok := os.LookupEnv("CLD_PATH"); ok {
-		dlUrl = fmt.Sprintf("%s%s", uribase, url.PathEscape(c))
-	}
-
-	if dlUrl == "" {
-		os.Exit(1)
-	}
-
-	for {
-		fmt.Println("Adding URL:", dlUrl)
-		ret, err := c.AddUri([]string{dlUrl}, dlname)
-		if err != nil {
-			time.Sleep(time.Second * 3)
-			continue
+		dlUrl := fmt.Sprintf("%s%s", uribase, url.PathEscape(c))
+		for {
+			fmt.Println("Adding URL:", dlUrl)
+			ret, err := aria2Client.AddUri([]string{dlUrl}, dlname)
+			if err != nil {
+				fmt.Println("error occur, wait 3", err)
+				time.Sleep(time.Second * 3)
+				continue
+			}
+			fmt.Printf("ret: gid %s\n", ret)
+			break
 		}
-		fmt.Printf("ret: gid %s\n", ret)
-		break
+	} else {
+		fmt.Println("CLD_PATH not found")
 	}
 }
